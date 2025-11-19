c49118adbc1528808e93d5d38dc37a4dc4a4a8aa
FILLIN-RX <djeutchouruxel@gmail.com>
2025-11-19 10:30:10 +0100

    first hello world

--- learning_app/lib/main.dart
+++ learning_app/lib/main.dart
@@ -1,59 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
+//Types of widget
+//1.StatelessWidget
 
+//2.StatefulWidget
+//3.InheritedWidget
+
+//state
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: StaticPage(),
-    );
-  }
-}
-
-class StaticPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 202, 190, 53),
-      appBar: AppBar(title: Text("My Static Flutter Page")),
-      body: Padding(
-        padding: EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Welcome to Flutter!",
-              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 16),
-            Text(
-              "This is a static page, similar to HTML content.",
-              style: TextStyle(fontSize: 18, color: Colors.grey[700]),
-            ),
-            SizedBox(height: 24),
-            Text(
-              "Section 1",
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-            ),
-            Text(
-              "Some static content for section 1...",
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(height: 16),
-            Text(
-              "Section 2",
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-            ),
-            Text(
-              "More static content here, no interaction.",
-              style: TextStyle(fontSize: 16),
-            ),
-          ],
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello world'),
         ),
       ),
     );
   }
 }
+
+//1.material app
+//2.cupertino app
