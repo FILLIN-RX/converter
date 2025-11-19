9f4116ab02fcdd0d65bf688165fc479e4ed9e560
FILLIN-RX <djeutchouruxel@gmail.com>
2025-11-19 10:34:19 +0100

    first hello world

---
+++ build/CMakeFiles/learning_app.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ruxel/project/converter/learning_app/linux/flutter/generated_plugin_registrant.cc" "CMakeFiles/learning_app.dir/flutter/generated_plugin_registrant.cc.o" "gcc" "CMakeFiles/learning_app.dir/flutter/generated_plugin_registrant.cc.o.d"
+  "/home/ruxel/project/converter/learning_app/linux/main.cc" "CMakeFiles/learning_app.dir/main.cc.o" "gcc" "CMakeFiles/learning_app.dir/main.cc.o.d"
+  "/home/ruxel/project/converter/learning_app/linux/my_application.cc" "CMakeFiles/learning_app.dir/my_application.cc.o" "gcc" "CMakeFiles/learning_app.dir/my_application.cc.o.d"
+  "" "intermediates_do_not_run/learning_app" "gcc" "CMakeFiles/learning_app.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/learning_app.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learning_app.dir/link.d"
+  "CMakeFiles/learning_app.dir/flutter/generated_plugin_registrant.cc.o"
+  "CMakeFiles/learning_app.dir/flutter/generated_plugin_registrant.cc.o.d"
+  "CMakeFiles/learning_app.dir/main.cc.o"
+  "CMakeFiles/learning_app.dir/main.cc.o.d"
+  "CMakeFiles/learning_app.dir/my_application.cc.o"
+  "CMakeFiles/learning_app.dir/my_application.cc.o.d"
+  "intermediates_do_not_run/learning_app"
+  "intermediates_do_not_run/learning_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/learning_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
